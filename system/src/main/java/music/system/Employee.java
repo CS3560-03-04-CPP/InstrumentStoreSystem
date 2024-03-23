6cc2d0dc671c1d3131f0d998fb265e291793570c
rcorv20 <164258114+rcorv20@users.noreply.github.com>
2024-03-22 21:50:42 -0700

    Add files via upload

---
+++ system/src/main/java/music/system/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+
+    private int employeeID;
+    private String name;
+    private String position;
+
+    // Constructor
+    public Employee(int employeeID, String name, String position) {
+
+    }
+
+}
