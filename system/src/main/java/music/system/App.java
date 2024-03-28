c0846b4f5b7ee2b26e6d8d7468c66a788372bfac
jethpson <jethpson@gmail.com>
2024-03-27 21:17:34 -0700

    Adding Comments to JavaFX files.

--- system/src/main/java/music/system/App.java
+++ system/src/main/java/music/system/App.java
@@ -12,17 +12,23 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- * JavaFX App
+ * JavaFX App: This Handles the initialization of javaFX and the properties of the windows created through-out the system. 
  */
 public class App extends Application {
 
+    // Static fields for storing scene and primary stage
     private static Scene scene;
     private static Stage primaryStage;
 
     @Override
     public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
+
         primaryStage = stage;
+
+        // Loading the SignInPage FXML file and creating a scene
         scene = new Scene(loadFXML("SignInPage"), 600, 300);
+
+         // Setting stage properties
         stage.setMaxHeight(400);
         stage.setMaxWidth(660);
         stage.setMinHeight(400);
@@ -33,6 +39,7 @@ public class App extends Application {
         stage.show();
     }
 
+    // Method to set the root of the scene
     static void setRoot(String fxml) throws IOException {
         Parent root = loadFXML(fxml);
         scene.setRoot(root);
@@ -48,17 +55,20 @@ public class App extends Application {
         }
     }
 
+    // Method to center the stage on the screen
     private void centerStageOnScreen(Stage stage) {
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         stage.setX((screenBounds.getWidth() - stage.getWidth()) / 2);
         stage.setY((screenBounds.getHeight() - stage.getHeight()) / 2);
     }
 
+    // Method to load FXML file
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
+    // Main method to launch the application
     public static void main(String[] args) {
         launch();
     }
