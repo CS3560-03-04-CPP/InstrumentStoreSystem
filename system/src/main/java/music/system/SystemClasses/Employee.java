40919988373ab16a557ee49059cc6820ac57b419
rcorv20 <164258114+rcorv20@users.noreply.github.com>
2024-04-19 16:06:53 -0700

    Add files via upload
    Added getters and setters to the classes, and imported javafx libraries

--- system/src/main/java/music/system/SystemClasses/Employee.java
+++ system/src/main/java/music/system/SystemClasses/Employee.java
@@ -1,5 +1,17 @@
 package music.system.SystemClasses;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Date;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Employee {
 
     private int employeeID;
@@ -9,6 +21,34 @@ public class Employee {
     // Constructor
     public Employee(int employeeID, String name, String position) {
 
+        this.employeeID = employeeID;
+        this.name = name;
+        this.position = position;
+    }
+
+    // Getters and setters
+    public int getID() {
+        return employeeID;
+    }
+
+    public void setID(int id) {
+        this.employeeID = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String pos) {
+        this.position = pos;
     }
 
 }
