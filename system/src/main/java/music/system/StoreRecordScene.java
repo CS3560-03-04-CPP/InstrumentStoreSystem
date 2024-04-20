13ee2689d7e12e5e6ce3d3d6799872db0f83a4ad
Tyler Nguyen-Uyemori <heatwave502@gmail.com>
2024-04-19 20:35:13 -0700

    Updated StoreRecord.java and Created StoreRecordScene.java

---
+++ system/src/main/java/music/system/StoreRecordScene.java
@@ -0,0 +1,105 @@
+package music.system;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import music.system.SystemClasses.StoreRecord;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class StoreRecordScene {
+
+    @SuppressWarnings("unchecked")
+    public static void displayStoreRecords() {
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Store Records");
+
+        TableView<StoreRecord> tableView = new TableView<>();
+
+        TableColumn<StoreRecord, String> invoiceNumberColumn = new TableColumn<>("Invoice Number");
+        invoiceNumberColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getInvoiceNumber()));
+
+        TableColumn<StoreRecord, Date> dateColumn = new TableColumn<>("Date");
+        dateColumn.setCellValueFactory(cellData -> {
+            SimpleObjectProperty<Date> property = new SimpleObjectProperty<>();
+            property.setValue(new java.sql.Date(cellData.getValue().getDate().getTime()));
+            return property;
+        });
+
+        tableView.getColumns().addAll(invoiceNumberColumn, dateColumn);
+
+        // TextField for search bar
+        TextField searchField = new TextField();
+        searchField.setPromptText("Search by Invoice Number");
+
+        // Add listener to react to user input in the search bar
+        searchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filterRecords(tableView, newValue);
+        });
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(searchField, tableView);
+
+        Scene scene = new Scene(vbox, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        populateStoreRecords(tableView);
+    }
+
+    private static void populateStoreRecords(TableView<StoreRecord> tableView) {
+        try {
+            // Establish connection to MySQL database
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Instrument_Store_System", "username", "password");
+
+            // Define SQL query to select all records from the database
+            String query = "SELECT * FROM store_records";
+
+            // Create a Statement
+            Statement statement = connection.createStatement();
+
+            // Execute the query and get the ResultSet
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // Populate TableView with the results
+            ObservableList<StoreRecord> storeRecords = FXCollections.observableArrayList();
+            while (resultSet.next()) {
+                StoreRecord storeRecord = new StoreRecord(
+                        resultSet.getString("invoice_number"),
+                        resultSet.getDate("date")
+                );
+                storeRecords.add(storeRecord);
+            }
+
+            tableView.setItems(storeRecords);
+
+            // Close resources
+            System.out.println("Connection closed to the database: StoreRecord Scene");
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to filter records based on invoice number
+    private static void filterRecords(TableView<StoreRecord> tableView, String invoiceNumber) {
+        ObservableList<StoreRecord> filteredData = FXCollections.observableArrayList();
+        for (StoreRecord record : tableView.getItems()) {
+            if (record.getInvoiceNumber().contains(invoiceNumber)) {
+                filteredData.add(record);
+            }
+        }
+        tableView.setItems(filteredData);
+    }
+}
--- system/src/main/java/music/system/SystemClasses/StoreRecord.java
+++ system/src/main/java/music/system/SystemClasses/StoreRecord.java
@@ -1,77 +1,71 @@
 package music.system.SystemClasses;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Date;
 
 public class StoreRecord {
-    
-    private String invoiceNumber; // Unique identifier for the purchase record
-    private Date date; // Date of the purchase
 
+    // Attributes
+    private String invoiceNumber;             // Unique invoice number for the store record
+    private Date date;                        // Date of the store record
+
+    // Constructor
     public StoreRecord(String invoiceNumber, Date date) {
         this.invoiceNumber = invoiceNumber;
         this.date = date;
     }
 
-     // Getters and Setters
+    // Save to MySQL method
+    public void saveToMySQL() {
+        try {
+            // Establish connection to MySQL database
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Instrument_Store_System", "username", "password");
 
-    /**
-     * Gets the invoice number of the purchase record.
-     *
-     * @return The invoice number
-     */
+            // Define SQL query to insert store record data into the database
+            String query = "INSERT INTO store_records (invoice_number, date) VALUES (?, ?)";
+
+            // Create PreparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, invoiceNumber);
+            preparedStatement.setDate(2, new java.sql.Date(date.getTime()));
+
+            // Execute the insert query
+            preparedStatement.executeUpdate();
+
+            // Close resources
+            System.out.println("Connection closed to the database: StoreRecord");
+            preparedStatement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Getters and Setters
     public String getInvoiceNumber() {
         return invoiceNumber;
     }
 
-    /**
-     * Sets the invoice number of the purchase record.
-     *
-     * @param invoiceNumber The invoice number to set
-     */
     public void setInvoiceNumber(String invoiceNumber) {
         this.invoiceNumber = invoiceNumber;
     }
 
-    /**
-     * Gets the date of the purchase record.
-     *
-     * @return The date of the purchase
-     */
     public Date getDate() {
         return date;
     }
 
-    /**
-     * Sets the date of the purchase record.
-     *
-     * @param date The date of the purchase to set
-     */
     public void setDate(Date date) {
         this.date = date;
     }
 
-    //Extra methods For Debugging.
-
+    // Override toString method to provide a string representation of the StoreRecord object
     @Override
     public String toString() {
-        return "PurchaseRecord{" +
+        return "StoreRecord{" +
                 "invoiceNumber='" + invoiceNumber + '\'' +
                 ", date=" + date +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        StoreRecord that = (StoreRecord) o;
-        return invoiceNumber.equals(that.invoiceNumber) && date.equals(that.date);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = invoiceNumber.hashCode();
-        result = 31 * result + date.hashCode();
-        return result;
     }
 }
